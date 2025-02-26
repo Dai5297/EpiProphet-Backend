1d53fda026df16725ae6f82a83a523fe1a4cb797
Dai52 <1924517451@qq.com>
2025-02-26 21:54:30 +0800

    初始化项目结构

---
+++ epi-web/src/main/java/com/dai/EpiWebApplication.java
@@ -0,0 +1,13 @@
+package com.dai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpiWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpiWebApplication.class, args);
+    }
+
+}
